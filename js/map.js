f573160e0b862439311d2459dd653a90c65e74ec
Takuji Okubo <takuji.okubo@gmail.com>
2019-12-22 02:25:29 +0000

    Added InfoWindow

--- js/map.js
+++ js/map.js
@@ -1,6 +1,7 @@
 getMyPosition();
 
 var map;
+var service;
 function myMap(lat, lon) {
     console.log(lat, lon);
     var currentPosition = new google.maps.LatLng(lat, lon);
@@ -16,12 +17,11 @@ function myMap(lat, lon) {
         type: ['bar']
     }
     service = new google.maps.places.PlacesService(map);
-    //service.textSearch(request, callback);
 
-    service.nearbySearch(request, callback);
+    service.nearbySearch(request, callbackNearbySearch);
 };
 
-function callback(results, status) {
+function callbackNearbySearch(results, status) {
     if (status != google.maps.places.PlacesServiceStatus.OK) {
         return;
     } else {
@@ -49,11 +49,45 @@ function createMarkers(places) {
             position: place.geometry.location
         });
 
+        addInfoListener(marker, place);
         bounds.extend(place.geometry.location);
     }
     map.fitBounds(bounds);
 }
 
+
+
+//https://stackoverflow.com/questions/5868903/marker-content-infowindow-google-maps
+// You must put this outside the for loop. Otherwise, you will always have place[20]. 
+
+function addInfoListener(marker, place) {
+    google.maps.event.addListener(marker, 'click', function () {
+        // console.log("inside addListener", marker);
+        var request = { reference: place.reference };
+        // getDetails() must be called only when mouse is clicked.
+        // If you call many times consecutively, access will be denied. 
+        service.getDetails(request, function (place, status) {
+            // console.log("callbackGetDetails(place: " + place, " status: ", status, ")");
+            console.log("callbackGetDetails(place: " , place);
+
+            var photoUrl="";
+            if (typeof place.photos !== 'undefined') { 
+                photoUrl = place.photos[0].getUrl();
+            } 
+            // console.log("PhotoURL: "+ photoUrl);
+
+            contentString = place.name + "<br>" +
+                            place.formatted_phone_number + "<br>" + place.formatted_address + "<br>" +
+                            "<img src='" + photoUrl + "' width=250px><br>" +
+                            "<a href='" + place.url +"' target='_blank'>Google Map</a>";
+            //console.log(contentString);
+            var infowindow = new google.maps.InfoWindow({
+                content: contentString
+            });
+            infowindow.open(map, marker);
+        }) // end of getDetails()
+    }) // ebd of addListener()
+}
 
 
 function getMyPosition() {
